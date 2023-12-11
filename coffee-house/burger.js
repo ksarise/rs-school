232b74f3329315c43f8ecf7ea27ede975d2cccd3
Sergey Kravchenko <sergkrav.social@gmail.com>
2023-12-12 03:44:31 +0700

    feat:add burger open/close logic

---
+++ coffee-house/burger.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const burger = document.querySelector(".burger-menu");
+    const nav = document.querySelector(".nav-container");
+    const lineBot = document.querySelector(".burger-line-bottom");
+    const lineTop = document.querySelector(".burger-line-top");
+
+
+    burger.addEventListener("click",openBurger);
+    function openBurger () {
+        nav.classList.toggle("open");
+        lineBot.classList.toggle("cross");
+        lineTop.classList.toggle("cross");
+        if (nav.classList.contains("open")=true) {
+                nav.style.opacity = "1";
+
+        } else {
+            nav.style.opacity ="0";
+        }
+    }
+});
