ef73b280283d28a846095077e51e5c213a5c32a2
Sergey Kravchenko <sergkrav.social@gmail.com>
2023-12-12 07:50:55 +0700

    fix:fixing carousel controls

--- coffee-house/script.js
+++ coffee-house/script.js
@@ -1,30 +1,53 @@
 document.addEventListener("DOMContentLoaded", function () {
     const carousel = document.querySelector(".carousel");
-    const progressBar = document.querySelector(".favorite-coffee-controls");
+    const progressBars = document.querySelectorAll(".favorite-coffee-control");
     const nextButton = document.getElementById("right");
     const prevButton = document.getElementById("left");
-    
+    const slider = document.querySelector(".favorite-coffee-row-slider");
+
     const slides = Array.from(carousel.children);
-    const bars = Array.from(progressBar.children);
+ 
     console.log(slides);
-    console.log(bars);
+    console.log(progressBars);
 
     prevButton.addEventListener("click", prevPicture);
     nextButton.addEventListener("click", nextPicture);
+    slider.addEventListener("mouseenter",stopAuto);
+    slider.addEventListener("mouseleave",slideAuto);
     let currentIndex = 0;
+    let autoInt;
+
+    slideAuto();
+    nullActiveBar();
+    function slideAuto () {
+        autoInt = setInterval(() => {
+            nextPicture();
+        },6000);
+    }
+    function stopAuto () {
+        clearInterval(autoInt);
+    }
+    function restartAuto () {
+        clearInterval(autoInt);
+        slideAuto();
+    }
 
     function moveSlider() {
         const newPos = -currentIndex * 100;
         for (let i = 0; i < slides.length; i++) {
           const slide = slides[i];
+          const bar = progressBars[i];
           slide.style.transform = `translateX(${newPos}%)`;
             if (i=== currentIndex) {
-                bars[i].classList.add("active");
+                bar.classList.add("active");
             } else {
-                bars[i].classList.remove("active");
+                bar.classList.remove("active");
             }
         }
       }
+    function nullActiveBar () {
+        progressBars[0].classList.add("active");
+    }
     function prevPicture() {
         if (currentIndex > 0) {
           currentIndex--;
@@ -41,4 +64,6 @@ document.addEventListener("DOMContentLoaded", function () {
     }
     moveSlider();
     }
+    
+
 });
