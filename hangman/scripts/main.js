a9fdc0fce6f35d0fd81160dc64c028b28bc5380f
Sergey Kravchenko <sergkrav.social@gmail.com>
2024-01-16 04:28:01 +0700

    feat: add basic dom layout

--- hangman/scripts/main.js
+++ hangman/scripts/main.js
@@ -1,8 +1,20 @@
 import {generateElement} from './generateElement.js';
 document.addEventListener("DOMContentLoaded", function (){
- const body = document.querySelector("body");
- const header = generateElement("div", "header", body, "header");
- const h = generateElement("h1", "title", header, "Hangman");
- const picture = generateElement("img", "img1", header);
+  const body = document.querySelector("body");
+  const header = generateElement("header", "header", body, "header");
+  const h = generateElement("h1", "title", header, "Hangman");
+  const main = generateElement("main", "main", body);
+  const hangpic = generateElement("section", "pic-block", main);
+  const pictures = [
+    "./images/hangman.jpeg",
+  ]
+  const picture = generateElement("img", "img1", hangpic,"" ,pictures[0]);
+  const quiz = generateElement("section", "quiz-block", main);
+  const quest = generateElement("div", "quest-block", quiz, "quest");
+  const definition = generateElement("div", "def-block", quiz, "definition");
+  const fails = generateElement("div", "fails-block", quiz, "Incorrect answers:")
+  const keyboard = generateElement("section", "keyboard-block", main, "Keyboard");
+
+
 
 });
