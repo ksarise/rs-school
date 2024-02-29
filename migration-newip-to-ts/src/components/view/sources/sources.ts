18cf89f67f0ba3280241f424fbd1bf895a1b7ce6
Sergey Kravchenko <sergkrav.social@gmail.com>
2024-02-29 20:35:24 +0600

    fix: remove Non-null assertion operator from sources.ts

--- migration-newip-to-ts/src/components/view/sources/sources.ts
+++ migration-newip-to-ts/src/components/view/sources/sources.ts
@@ -5,14 +5,24 @@ class Sources {
     public draw(data: SourceResponse): void {
         const fragment: DocumentFragment = document.createDocumentFragment();
         const sourceItemTemp = document.querySelector<HTMLTemplateElement>('#sourceItemTemp');
-        data.sources.forEach((item: Source) => {
-            const sourceClone = sourceItemTemp!.content.cloneNode(true) as DocumentFragment;
-            sourceClone.querySelector<Element>('.source__item-name')!.textContent = item.name;
-            sourceClone.querySelector<Element>('.source__item')!.setAttribute('data-source-id', item.id!);
-            fragment.appendChild(sourceClone);
-        });
-
-        document.querySelector('.sources')!.append(fragment);
+        if (sourceItemTemp) {
+            data.sources.forEach((item: Source) => {
+                const sourceClone = sourceItemTemp.content.cloneNode(true) as DocumentFragment;
+                const sourceName: Element | null = sourceClone.querySelector<Element>('.source__item-name');
+                const sourceId: Element | null = sourceClone.querySelector<Element>('.source__item');
+                if (sourceName) {
+                    sourceName.textContent = item.name;
+                }
+                if (sourceId && item.id) {
+                    sourceId.setAttribute('data-source-id', item.id);
+                }
+                fragment.appendChild(sourceClone);
+            });
+            const sourceBlock = document.querySelector('.sources');
+            if (sourceBlock) {
+                sourceBlock.appendChild(fragment);
+            }
+        }
     }
 }
 
