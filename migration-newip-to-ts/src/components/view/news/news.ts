eead08f59f60a25ca1f8f2248d5ec1b453d1b2ed
Sergey Kravchenko <sergkrav.social@gmail.com>
2024-02-29 20:26:04 +0600

    fix: remove Non-null assertion operator from news.ts

--- migration-newip-to-ts/src/components/view/news/news.ts
+++ migration-newip-to-ts/src/components/view/news/news.ts
@@ -11,30 +11,50 @@ class News {
         const newsItemTemp = document.querySelector<HTMLTemplateElement>('#newsItemTemp');
 
         news.forEach((item, idx) => {
-            const newsClone = newsItemTemp!.content.cloneNode(true) as DocumentFragment;
-
-            if (idx % 2) newsClone.querySelector('.news__item')!.classList.add('alt');
-
-            newsClone.querySelector<HTMLElement>('.news__meta-photo')!.style.backgroundImage = `url(${
-                item.urlToImage || '350931d95a9f7ad6376d.png'
-            })`;
-            newsClone.querySelector<HTMLElement>('.news__meta-author')!.textContent = item.author || item.source.name;
-            newsClone.querySelector<HTMLElement>('.news__meta-date')!.textContent = item.publishedAt
-                .slice(0, 10)
-                .split('-')
-                .reverse()
-                .join('-');
-
-            newsClone.querySelector<HTMLElement>('.news__description-title')!.textContent = item.title;
-            newsClone.querySelector<HTMLElement>('.news__description-source')!.textContent = item.source.name;
-            newsClone.querySelector<HTMLElement>('.news__description-content')!.textContent = item.description;
-            newsClone.querySelector<HTMLElement>('.news__read-more a')!.setAttribute('href', item.url);
-
+            const newsClone = newsItemTemp!.content.cloneNode(true) as HTMLElement;
+            const newsCloneElement: Element | null = newsClone.querySelector('.news__item');
+            if (newsCloneElement && idx % 2) {
+                newsCloneElement.classList.add('alt');
+            }
+            const newsMetaPhoto: HTMLElement | null = newsClone.querySelector('.news__meta-photo');
+            if (newsMetaPhoto) {
+                if (item.urlToImage !== null) {
+                    newsMetaPhoto.style.backgroundImage = `url(${item.urlToImage})`;
+                } else {
+                    newsMetaPhoto.classList.add('place');
+                }
+            }
+            const newsMetaAuthor: HTMLElement | null = newsClone.querySelector('.news__meta-author');
+            if (newsMetaAuthor) {
+                newsMetaAuthor.textContent = item.author || item.source.name;
+            }
+            const newsMetaDate: HTMLElement | null = newsClone.querySelector('.news__meta-date');
+            if (newsMetaDate) {
+                newsMetaDate.textContent = item.publishedAt.slice(0, 10).split('-').reverse().join('-');
+            }
+            const newsMetaTitle: HTMLElement | null = newsClone.querySelector('.news__description-title');
+            if (newsMetaTitle) {
+                newsMetaTitle.textContent = item.title;
+            }
+            const newsMetaSource: HTMLElement | null = newsClone.querySelector('.news__description-source');
+            if (newsMetaSource) {
+                newsMetaSource.textContent = item.source.name;
+            }
+            const newsMetaContent: HTMLElement | null = newsClone.querySelector('.news__description-content');
+            if (newsMetaContent) {
+                newsMetaContent.textContent = item.description;
+            }
+            const newsReadmore: HTMLElement | null = newsClone.querySelector('.news__read-more');
+            if (newsReadmore) {
+                newsReadmore.setAttribute('href', item.url);
+            }
             fragment.append(newsClone);
         });
-
-        document.querySelector<HTMLElement>('.news')!.innerHTML = '';
-        document.querySelector<HTMLElement>('.news')!.appendChild(fragment);
+        const newsBlock: HTMLElement | null = document.querySelector<HTMLElement>('.news');
+        if (newsBlock) {
+            newsBlock.innerHTML = '';
+            newsBlock.appendChild(fragment);
+        }
     }
 }
 
