9f0aa2400579fa961f88eadbab0b2081922bdf78
ksarise <sergkrav.social@gmail.com>
2024-03-21 09:20:08 +0500

    fix: fix validation order for clear check (#107)

--- rss-puzzle/src/app/pages/login-form/validation.ts
+++ rss-puzzle/src/app/pages/login-form/validation.ts
@@ -2,10 +2,10 @@ export default function Validator(value: string, nameLength: number): string {
   let message: string = '';
   if (nameLength > value.length) {
     message = `Minimum ${nameLength} letters`;
-  } else if (!/^[A-Z]/.test(value[0])) {
-    message = 'The first letter must be uppercase';
   } else if (!/^[A-Za-z-]+$/.test(value)) {
     message = 'Only English alphabet letters or the hyphen';
+  } else if (!/^[A-Z]/.test(value[0])) {
+    message = 'The first letter must be uppercase';
   } else {
     message = 'ok';
   }
