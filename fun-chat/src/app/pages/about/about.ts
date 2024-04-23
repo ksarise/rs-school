7dc00240c4597a497a90a4fc3437c66829b4b758
ksarise <sergkrav.social@gmail.com>
2024-04-23 20:08:28 +0500

    Feat/RSS-FC-09_AboutPage (#121)

--- fun-chat/src/app/pages/about/about.ts
+++ fun-chat/src/app/pages/about/about.ts
@@ -1,6 +1,9 @@
 import BaseComponentGenerator from '../../components/base-component';
 import ButtonElement from '../../components/button';
+import './about.css';
 
+const aboutInfo: string =
+  'Fun-chat is a delightful, user-friendly chat application designed for spontaneous conversations and light-hearted interactions. With its intuitive interface and array of playful features, Fun-chat fosters a vibrant community where users can connect, share jokes, and engage in lively discussions effortlessly.';
 export default class AboutPage {
   private about: HTMLElement;
 
@@ -11,13 +14,34 @@ export default class AboutPage {
       tag: 'div',
       className: 'about',
     });
+    const aboutTitleGen = new BaseComponentGenerator({
+      tag: 'h2',
+      className: 'about__title',
+      content: 'Fun-chat',
+    });
+    const aboutInfoGen = new BaseComponentGenerator({
+      tag: 'p',
+      className: 'about__info',
+      content: aboutInfo,
+    });
+    const aboutAuthorGen = new BaseComponentGenerator({
+      tag: 'a',
+      className: 'about__author',
+      content: 'Sergey Kravchenko',
+      attributes: { href: 'https://github.com/ksarise' },
+    });
     this.backBtn = new ButtonElement(
       'backBtn',
       'Back',
       { type: 'button' },
       () => {}
     );
-    aboutGen.appendElement(this.backBtn.getButton());
+    aboutGen.appendChildren([
+      aboutTitleGen,
+      aboutInfoGen,
+      aboutAuthorGen,
+      this.backBtn.getButton(),
+    ]);
     this.about = aboutGen.getElement() as HTMLElement;
   }
 
